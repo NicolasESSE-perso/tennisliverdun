3b98df2869f0d734ab945b8fb25f5afc0196ed93
Nicolas Esse <nicolas.esse@gmail.com>
2023-09-02 18:22:29 +0200

    inscriptions

--- src/Components/Inscriptions.js
+++ src/Components/Inscriptions.js
@@ -5,14 +5,25 @@ import BoutonTenUp from "../Components/Boutons/BoutonTenUp";
 export default function Inscriptions() {
   return (
     <div className={Style.Inscriptions}>
-      <p className="Titre2">Inscriptions 2022 - 2023</p>
-      Bonjour, Nous vous proposons de venir nous retrouver pour la nouvelle
-      saison 2024, lors des jours d'inscription suivant: - Samedi 2 septembre de
-      10h à 12h - Vendredi 8 septembre de 18h à 20h. - Dimanche 10 septembre
-      lors de la journée des associations ( avec animations) de 9h30 à 17h. Les
-      inscriptions se dérouleront dans notre salle couverte derrière l'espace
-      Champagne. Pour tout besoin d'informations vous pouvez contacter Séveirne
-      au 06 81 85 19 73 Très sportivement. R BALANDRET Le Président.
+      <p className="Titre2">Inscriptions 2023 - 2024</p>
+      <p>
+        Bonjour, Nous vous proposons de venir nous retrouver pour la nouvelle
+        saison 2024, lors des jours d'inscription suivant:
+      </p>
+      <ul>
+        <li> Samedi 2 septembre de 10h à 12h</li>
+        <li>Vendredi 8 septembre de 18h à 20h.</li>
+        <li>
+          Dimanche 10 septembre lors de la journée des associations ( avec
+          animations) de 9h30 à 17h.
+        </li>
+      </ul>
+      <p>
+        Les inscriptions se dérouleront dans notre salle couverte derrière
+        l'espace Champagne. Pour tout besoin d'informations vous pouvez
+        contacter Séverine au 06 81 85 19 73
+      </p>
+      <p>Très sportivement. R BALANDRET Le Président.</p>
       <br />
       <BoutonTenUp
         hauteur="50px"
